be69cb5350a33ae62e3f85e1a287ddd3088151c0
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-01 16:32:42 -0500

    Add an addMove method to the class that when called will push the chosen position into the added property this.moves

--- player.js
+++ player.js
@@ -3,9 +3,14 @@ class Player {
     this.id = id;
     this.cursor = cursor;
     this.wins = 0;
+    this.moves = [];
   }
 
   increaseWins() {
     this.wins += 1;
+  }
+
+  addMove(id) {
+    this.moves.push(id);
   }
 }
