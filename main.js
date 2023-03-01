98c4e63805316e297c1aa5eef7f81547a32381fb
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-02-28 20:38:56 -0500

    Add query selectors for all the tic tac squares and add the data model

---
+++ main.js
@@ -0,0 +1,22 @@
+/* query selectors */
+var squareOne = document.querySelector('#squareOne');
+var squareTwo = document.querySelector('#squareTwo');
+var squareThree = document.querySelector('#squareThree');
+var squareFour = document.querySelector('#squareFour');
+var squareFive = document.querySelector('#squareFive');
+var squareSix = document.querySelector('#squareSix');
+var squareSeven = document.querySelector('#squareSeven');
+var squareEight = document.querySelector('#squareEight');
+var squareNine = document.querySelector('#squareNine');
+
+/* Data Model */
+
+var game = new Game();
+var playerOne = new Player('one', '🎃');
+var playerTwo = new Player('two', '👻')
+
+/* Event Listeners */
+
+/* Event Handlers */
+
+/* Functions */
