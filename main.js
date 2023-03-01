2b3dfee2fc65549f96d79548acf08bcc35d64efc
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-01 16:36:16 -0500

    Update data model, add event listener for the game board and corresponding function addIcon

--- main.js
+++ main.js
@@ -8,15 +8,33 @@ var squareSix = document.querySelector('#squareSix');
 var squareSeven = document.querySelector('#squareSeven');
 var squareEight = document.querySelector('#squareEight');
 var squareNine = document.querySelector('#squareNine');
-
+var gameBoard = document.querySelector('#boardGame');
 /* Data Model */
 
 var game = new Game();
 var playerOne = new Player('one', '🎃');
-var playerTwo = new Player('two', '👻')
+var playerTwo = new Player('two', '👻');
+game.addPlayers(playerOne);
+game.addPlayers(playerTwo);
+game.changePlayer();
 
 /* Event Listeners */
 
+gameBoard.addEventListener('click', addIcon);
+
 /* Event Handlers */
+
+function addIcon(event) {
+  var playerOneOccupy = playerOne.moves.includes(event.target.id);
+  var playerTwoOccupy = playerTwo.moves.includes(event.target.id);
+  if (game.playerTurn === "one" && !playerOneOccupy && !playerTwoOccupy) {
+    playerOne.addMove(event.target.id);
+    game.changePlayer();
+  } else if (game.playerTurn === "two" && !playerTwoOccupy && !playerOneOccupy){
+    playerTwo.addMove(event.target.id);
+    game.changePlayer();
+  }
+ 
+}
 
 /* Functions */
