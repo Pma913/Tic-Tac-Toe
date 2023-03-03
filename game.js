b520e59946f0ed76a38064f90669e0f1c036546a
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-03 15:32:48 -0500

    Add a class to the player-turn-container div in the event of a win and remove it after reset. This will romove the ability to keep adding characters after a game is one and before the board resets

--- game.js
+++ game.js
@@ -12,26 +12,12 @@ class Game {
   }
 
   changePlayer() {
-    // var playerTurnDisplay = document.querySelector('#innerTextLine');
     if (this.playerTurn === 'one') {
       this.playerTurn = 'two';
-      // playerTurnDisplay.innerText = `${playerTwo.cursor}`;
-      // playerTurnDisplay.innerText = `${playerTwo.cursor}`;
     } else {
       this.playerTurn = 'one';
-      // playerTurnDisplay.innerText = `${playerOne.cursor}`;
-      // playerTurnDisplay.innerText = `${playerOne.cursor}`;
     }
   }
-
-  // displayPlayer() {
-  //   var playerTurnDisplay = document.querySelector('#innerTextLine');
-  //   if (this.playerTurn === 'one') {
-  //     playerTurnDisplay.innerText = `${playerOne.cursor}`;
-  //   } else {
-  //     playerTurnDisplay.innerText = `${playerTwo.cursor}`;
-  //   }    
-  // }
 
   checkForWin(player) {
       this.movesLeft -= 1;
@@ -51,6 +37,7 @@ class Game {
         setTimeout(displayPlayer, 3000);
         this.movesLeft = 9;
         this.winCounter();
+        winDrawDisplay.classList.add('end-game');
       } else if (this.movesLeft === 0) {
         winDrawDisplay.innerHTML = `<h2>It be a draw!</h2>`;
         setTimeout(this.resetBoard, 3000);
@@ -66,6 +53,7 @@ class Game {
     for (var i = 0; i < squares.length; i++) {
       squares[i].innerHTML = '';
     }
+    winDrawDisplay.classList.remove('end-game');
     playerOne.moves.length = 0;
     playerTwo.moves.length = 0;
     winDrawDisplay.innerHTML = `<h2 class="player-turn"">It's <span class="inner-text-display" id="innerTextLine"></span>'s turn</h2>`;
