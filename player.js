645f5abc4fdcbe7ffa0acdeb4177e2d09d7a67a9
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-04 16:01:38 -0500

    Add restoreMoves method to restore game pieces after refresh

--- player.js
+++ player.js
@@ -13,4 +13,10 @@ class Player {
   addMove(id) {
     this.moves.push(id);
   }
+
+  restoreMoves() {
+    for (var i = 0; i < this.moves.length; i++) {
+      document.getElementById(this.moves[i]).innerHTML = `<h2 class="player-icon">${this.cursor}</h2>`
+    }
+  }
 }
