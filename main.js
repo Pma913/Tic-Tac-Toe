d03c1d210c774c55abac4eeb01c891e55a338756
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-04 15:21:48 -0500

    Create two functions, assignDataModel and CreateDataModel so they can make checkLocalStorage function DRY and consolidated

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 /* query selectors */
 
-var playerOneWins = document.querySelector('#playerOneWins')
-var playerTwoWins = document.querySelector('#playerTwoWins')
+var playerOneWins = document.querySelector('#playerOneWins');
+var playerTwoWins = document.querySelector('#playerTwoWins');
 var gameBoard = document.querySelector('#boardGame');
 var winDrawDisplay = document.querySelector('#displayTurn');
 /* Data Model */
-
+var savedGame = JSON.parse(localStorage.getItem('game'));
 var game;
 var playerOne;
 var playerTwo;
-checkLocalStorage()
-// var game = new Game();
-// var playerOne = new Player('one', 'X');
-// var playerTwo = new Player('two', 'O');
-// game.addPlayers(playerOne);
-// game.addPlayers(playerTwo);
-// displayPlayer();
+checkLocalStorage();
 
 /* Event Listeners */
 
@@ -43,39 +37,41 @@ function addIcon(event) {
     game.checkForWin(playerTwo);
   }
 
-  // game.changePlayer();
-  displayPlayer()
-  
+  displayPlayer();
 }
 
 function displayPlayer() {
-    var playerTurnDisplay = document.querySelector('#innerTextLine');
+  var playerTurnDisplay = document.querySelector('#innerTextLine');
 
-    if (game.playerTurn === 'one') {
-      playerTurnDisplay.innerText = `${playerOne.cursor}`;
-    } else {
-      playerTurnDisplay.innerText = `${playerTwo.cursor}`;
-    }
-  }  
-
-  function checkLocalStorage() {
-    if (localStorage.length === 1) {
-      var savedGame = JSON.parse(localStorage.getItem('game'));
-      console.log(savedGame)
-      game = new Game(savedGame.players, savedGame.playerTurn, savedGame.movesLeft);
-      console.log(game)
-      playerOne = new Player('one', 'X', savedGame.players[0].wins, savedGame.players[0].moves);
-      playerTwo = new Player('two', 'O', savedGame.players[1].wins, savedGame.players[1].moves);
-      game.addPlayers(playerOne);
-      game.addPlayers(playerTwo);
-      console.log(game)
-      displayPlayer();
-    } else {
-      game = new Game();
-      playerOne = new Player('one', 'X');
-      playerTwo = new Player('two', 'O');
-      game.addPlayers(playerOne);
-      game.addPlayers(playerTwo);
-      displayPlayer();
-    }
+  if (game.playerTurn === 'one') {
+    playerTurnDisplay.innerText = `${playerOne.cursor}`;
+  } else {
+    playerTurnDisplay.innerText = `${playerTwo.cursor}`;
   }
+}  
+
+function checkLocalStorage() {
+  if (localStorage.length === 1) {
+    assignDataModel();
+    displayPlayer();
+  } else {
+    createDataModel();
+    displayPlayer();
+  }
+}
+
+function assignDataModel() {
+  game = new Game(savedGame.players, savedGame.playerTurn, savedGame.movesLeft);
+  playerOne = new Player('one', 'X', savedGame.players[0].wins, savedGame.players[0].moves);
+  playerTwo = new Player('two', 'O', savedGame.players[1].wins, savedGame.players[1].moves);
+  game.addPlayers(playerOne);
+  game.addPlayers(playerTwo);
+}
+
+function createDataModel() {
+  game = new Game();
+  playerOne = new Player('one', 'X');
+  playerTwo = new Player('two', 'O');
+  game.addPlayers(playerOne);
+  game.addPlayers(playerTwo);
+}
