5f5fa85719ba0f6bda9e38214ee0e998016083d8
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-03-06 09:48:38 -0500

    Add freezeBoard function within resetBoard method

--- game.js
+++ game.js
@@ -69,7 +69,7 @@ class Game {
       squares[i].innerHTML = '';
     }
 
-    winDrawDisplay.classList.remove('end-game');
+    freezeBoard();
     winDrawDisplay.innerHTML = `<h2 class="player-turn"">It's <span class="inner-text-display" id="innerTextLine"></span>'s turn</h2>`;
   }
 
